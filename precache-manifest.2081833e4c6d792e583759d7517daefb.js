ab42a1efdc35b0f42e2c6a1309aba8e1da16521e
ldaucorp <larry@springrainit.com>
2020-06-07 01:34:47 +0600

    Updates

---
+++ precache-manifest.2081833e4c6d792e583759d7517daefb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14207255142c8d18f87ed423fab8ca81",
+    "url": "/about-me/index.html"
+  },
+  {
+    "revision": "588b37a45b6ca58583a1",
+    "url": "/about-me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "134f826c0bca5826aa70",
+    "url": "/about-me/static/js/2.9224a54d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/about-me/static/js/2.9224a54d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "588b37a45b6ca58583a1",
+    "url": "/about-me/static/js/main.859965c1.chunk.js"
+  },
+  {
+    "revision": "f8a5a897c5fd06aa09b9",
+    "url": "/about-me/static/js/runtime-main.f1961d8b.js"
+  }
+]);
